e82b1b4843a39d19af38c3fa89ce42128a37c1ed
Kylevb405 <kylevanburen405@gmail.com>
2020-10-13 17:45:43 -0400

    finished up to task 2

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 import { fifaData } from './fifa.js';
-console.log(fifaData);
+// console.log(fifaData);
 
 console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
+/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data */
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
 
+const teams2014 = fifaData.filter(item => item['Stage'] === 'Final' && item['Year'] === 2014);
+
+// console.log(teams2014);
+
+
+
+
+// (a) Home Team name for 2014 world cup final
+
+    // console.log(teams2014[0]['Home Team Name']);
+
+// (b) Away Team name for 2014 world cup final
+
+    // console.log(teams2014[0]['Away Team Name']);
+
+// (c) Home Team goals for 2014 world cup final
+
+    // console.log(teams2014[0]['Home Team Goals']);
+
+// (d) Away Team goals for 2014 world cup final
+
+    // console.log(teams2014[0]['Away Team Goals']);
+
+// (e) Winner of 2014 world cup final */
+
+    // console.log(teams2014[0]['Win conditions']);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(array) {
 
-    /* code here */
+    const finalsTeams = array.filter(key => key['Stage'] === 'Final')
+
+    console.log(finalsTeams);
 
 };
+
+console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
@@ -63,6 +88,22 @@ function getAverageGoals(/* code here */) {
 };
 
 getAverageGoals();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /// STRETCH 🥅 //
 
